7a5032ad9cdefca17376c12910a4e4b082f8820c
JackDarkWeb <jacknouatin1@gmail.com>
2019-08-07 07:10:47 +0300

    new deposity

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Course Javascript</title>
+</head>
+<body>
+
+<script async src="script.js"></script>
+</body>
+</html>
+
+
+
+<?php
