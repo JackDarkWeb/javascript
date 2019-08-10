63cd38cee09753e9c4d004453c5dfc77785abc26
JackDarkWeb <jacknouatin1@gmail.com>
2019-08-10 19:55:27 +0300

    new deposity

---
+++ dom.js
@@ -0,0 +1,134 @@
+class Dom{
+
+    static getId(name){
+        return document.getElementById(name);
+    }
+    static getClass(name){
+        let classes = [] ;
+        classes = document.getElementsByClassName(name);
+        return classes;
+    }
+    static getTag(name){
+        let tags = [];
+        tags = document.getElementsByTagName(name);
+        return tags;
+    }
+    static create(name, parent){
+        const elt = document.createElement(name);
+        return parent.appendChild(elt);
+    }
+
+    static getClassCount(name){
+        return this.getClass(name).length;
+    }
+    static getTagsCount(name){
+        return this.getTag(name).length;
+    }
+}
+
+
+let getForms = () =>{
+
+    let getId = document.getElementById('first_name');
+    let getClass = document.getElementsByClassName('last_name')[0];
+
+    const first_name = getId.value;
+    const last_name = getClass.value;
+
+    alert(`${first_name} et  ${last_name} `);
+};
+
+const contents = document.getElementsByClassName('content');
+//alert(Dom.getClassCount('content'));
+for(const content of contents){
+    //alert(content);
+}
+
+const tag = document.getElementsByTagName('p:first');
+//alert(tag);
+
+/*
+querySelector()  ne renvoie pas une liste des résultats, mais le premier élément qui correspond à la recherche.
+ */
+const query = document.querySelector('p > a');
+//alert(query);
+
+// MODIFIER UN DOM ###############
+
+    // Property innerHTML
+let elt = document.getElementById('innerHTML');
+elt.innerHTML = "<ul><li>Item 1</li><li>Item 2</li></ul>";
+
+// Property textContent
+let elmt = document.getElementById('textContent');
+elmt.textContent = "<ul><li>Item 3</li><li>Item 4</li></ul>";
+
+
+
+// MODIFIER LES CLASSES DUN ELEMENT
+
+const classLists = () => {
+
+    let getClass = document.getElementsByClassName('alert');
+
+    getClass[0].classList.remove('alert-success');
+    getClass[0].classList.add('alert-danger');
+
+    getClass[1].classList.remove('alert-info');
+    getClass[1].classList.add('alert-warning');
+};
+
+let element = document.getElementById('btn-success');
+let div = document.getElementById('col');
+element.style.backgroundColor = 'red';
+
+// MODIFIER LES ATTRIBUTES
+
+const img = () => {
+    let atr = document.getElementsByTagName('img');
+    atr[0].setAttribute('src', 'javascript.png');
+};
+
+
+const ShowHidePassword = () =>{
+
+    let badges = document.getElementsByClassName('badge');
+    let getId = document.getElementById('password');
+    const attr = getId.getAttribute('type');
+
+   if(attr === 'password'){
+
+       getId.setAttribute('type', 'text');
+       badges[0].classList.add('d-none');
+       badges[1].classList.remove('d-none');
+
+   }else{
+       getId.setAttribute('type', 'password');
+       badges[0].classList.remove('d-none');
+       badges[1].classList.add('d-none');
+   }
+
+};
+
+//AJOUT D UN ELEMENT CREER
+
+const newElt = document.createElement("input");
+elmt.appendChild(newElt);
+
+const addInput = (string) => {
+
+    let parent = Dom.getId('add');
+    let value = string.replace(/[.,\/#@!$%\^&\*;:{}=\-_`~()]/g, "");
+
+    Dom.getId('user').value = value;
+
+    const children = parent.children;
+
+    if(value.length === 8){
+        Dom.create('input', parent);
+        children[0].nextElementSibling.innerHTML = '';
+    }else{
+        children[0].nextElementSibling.innerHTML = 'Error';
+        parent.removeChild(children[2]);
+    }
+};
--- index.php
+++ index.php
@@ -7,10 +7,76 @@
 </head>
 <body>
 <div class="text-center mt-5">
-    <button class="btn btn-success">Tester</button>
+    <div class="row mt-5">
+        <div class="col-md-4">
+            <p id="content">Le formulaire</p>
+            <form method="post" action="" id="formulary">
+
+                <input  type="text" name="first_name" id="first_name" placeholder="First name"/><br/><br/>
+                <input  type="text" name="last_name" class="last_name" placeholder="Last name"/><br/><br/>
+                <button class="btn btn-success" onclick="getForms()">Tester</button>
+            </form>
+        </div>
+        <div class="col-md-4">
+
+            <div class="mt-5">
+                <div class="content">Content 1</div>
+                <div class="content">Content 2</div>
+                <div class="content">Content 3</div>
+                <div class="content">Content 4</div>
+                <div>
+                    <p><a href="">Le lien vers document.querySelector()</a> </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="mt-5" id="innerHTML"></div>
+            <div id="textContent"></div>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col-md-3">
+            <div class="alert alert-success">
+                <strong>Je suis heuruex</strong>
+            </div>
+            <div class="alert alert-info">
+                <strong>Je suis heuruex</strong>
+            </div>
+            <button class="btn btn-outline-info" onclick="classLists()">classList</button>
+        </div>
+
+        <div class="col-md-3" id="col">
+            <button class="btn btn-lg btn-success" id="btn-success"></button>
+            <button class="btn btn-lg btn-secondary" id="btn-secondary"></button>
+        </div>
+
+        <div class="col-md-3">
+            <img src="java.jpeg" alt="Java" class="rounded img-thumbnail" onclick="img()"/>
+        </div>
+        <div class="col-md-3">
+            <input type="password" name="password" id="password" /><span class="badge badge-danger"  style="cursor: pointer" onclick="ShowHidePassword()">Show</span><span class="badge badge-info d-none"  style="cursor: pointer" onclick="ShowHidePassword()">Hide</span>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-md-4" id="add">
+            <input type="text" name="user" id="user" placeholder="Enter your username" onkeyup="addInput(this.value)" />
+            <span class="text-danger"></span>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
 </div>
 
-<script async src="test_unitaire.js"></script>
+<script defer async src="dom.js"></script>
 </body>
 </html>
 
